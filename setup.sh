2f027855252e2e449629457d2dc15e5abf7a5b75
John Nolette <jnolette@transparent.com>
2019-01-10 10:57:44 -0500

    Adding basic setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+  sudo apt-get update
+  sudo apt-get install -y curl python python-pip python-dev build-essential
+fi
+if [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
+  if [ "$(which dnf)" ]; then
+    sudo dnf install -y curl python python-pip python-libs
+  else
+    # assume yum is available
+    sudo yum install -y curl python python-pip python-libs
+  fi
+fi
+
+sudo pip install --upgrade pip
+sudo pip install ansible
+curl -fsSL https://get.docker.com/ | sh
+ansible-playbook ansible/playbook.yml -vvv
